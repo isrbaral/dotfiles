68b8aaad621ce0014bbd94b095ada9bdeca9c6e2
isrbaral <3858343+isrbaral@users.noreply.github.com>
2023-05-18 09:19:54 -0500

    reconfigured remove dotfile

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -6,7 +6,7 @@ $Boxstarter.AutoLogin=$false
 # TODO: see how to improve install that by using chezmoi (choco install -y chezmoi)
 choco install -y git --params "/GitOnlyOnPath /WindowsTerminal"
 RefreshEnv
-if(Test-Path $env:USERPROFILE\dotfiles) {Remove-Item $env:USERPROFILE\dotfiles -Force -Recurse}
+if(Test-Path $env:USERPROFILE\dotfiles) {Get-ChildItem $env:USERPROFILE\dotfiles -Recurse | Remove-Item -Force}
 git clone https://github.com/isrbaral/dotfiles.git "$env:USERPROFILE\dotfiles"
 # Git configuration
 Remove-Item -Path "$env:USERPROFILE\.gitconfig" -Force
